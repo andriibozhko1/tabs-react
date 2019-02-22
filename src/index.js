54d37d9b8abd04b033e9527908bc27714234b03a
Andrii <andriibozhko1@gmail.com>
2019-02-22 18:59:59 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Tabs from "./component/Tabs";
+import Tab from "./component/Tab";
+import "./style/style.scss";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Tabs
+          onTabSelected={data => {
+            console.log(data.title, data.text);
+          }}
+        >
+          <Tab title="Tab 1">Some text 1</Tab>
+          <Tab title="Tab 2">Some text 2</Tab>
+          <Tab title="Tab 3">Some text 3</Tab>
+        </Tabs>
+        <Tabs>
+          <Tab title="Tab 1">asd text 1</Tab>
+          <Tab title="Tab 2">Some text 2</Tab>
+          <Tab title="Tab 3">Some text 3</Tab>
+        </Tabs>
+      </div>
+    );
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root"));
