88de39adcf37c1aca11015d5b2cb210dc07470ba
Andrii <andriibozhko1@gmail.com>
2019-02-22 19:05:53 +0200

    to gh-pages

---
+++ precache-manifest.6bf97fa410bac065d4e709fa5d00a202.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66d469d962620f4ef121",
+    "url": "/tabs-react/static/css/main.21f26416.chunk.css"
+  },
+  {
+    "revision": "66d469d962620f4ef121",
+    "url": "/tabs-react/static/js/main.66d469d9.chunk.js"
+  },
+  {
+    "revision": "08e37f32075bee95c42f",
+    "url": "/tabs-react/static/js/runtime~main.08e37f32.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/tabs-react/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "b03aebc3dc04d5dddc8849d700288381",
+    "url": "/tabs-react/index.html"
+  }
+];
